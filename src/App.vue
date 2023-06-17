23f56522aa8afececed5cb627cf22f25e5006708
kennethleanda <leandakenneth@gmail.com>
2023-06-17 23:08:43 +0800

    changes

--- src/App.vue
+++ src/App.vue
@@ -36,3 +36,14 @@
 
 </template>
 
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+
+</script>
+
