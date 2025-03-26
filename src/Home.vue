dbb7bf3a30a8f5c8acaaa2d885eab07c00ea26ba
kenneth <admin@k.local>
2025-03-26 09:49:40 +0800

    update

--- src/Home.vue
+++ src/Home.vue
@@ -4,12 +4,11 @@
   
     <div class="flex w-full h-screen items-center justify-center ">
       <div class="relative">
-        <h1 class="portfolio  font-sera z-20 text-center xl:text-[280px] lg:text-[250px] md:text-[200px] sm:text-[280px] min-[320px]:text-[280px] break-all min-[320px]:h-screen lg:h-auto uppercase font-black text-gunmetal m-0 p-0">
+        <h1 class="portfolio  font-sera z-20 text-center xl:text-[260px] lg:text-[250px] md:text-[200px] sm:text-[280px] min-[320px]:text-[280px] break-all min-[320px]:h-screen lg:h-auto uppercase font-black text-gunmetal m-0 p-0">
           Portfolio
         </h1>
         <p class="text-sm fixed font-medium bottom-10 right-10 m-4 text-un-blue tracking-tight">Design and Development by Kenneth Leanda</p>
         <p class="text-sm fixed font-medium top-10 left-10 m-4 text-un-blue tracking-tight">UI / UX Design</p>
-
       </div>
     </div>
   </div>
